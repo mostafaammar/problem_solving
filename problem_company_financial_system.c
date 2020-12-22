5b67acfe76cd57e721cb498295dd8ba54752d628
mostafaammar <mostafaammar@users.noreply.github.com>
2020-12-22 10:47:47 +0200

    Update problem_company_financial_system.c

--- problem_company_financial_system.c
+++ problem_company_financial_system.c
@@ -6,7 +6,7 @@ A company wants to create a bonus system for its employees .
 System will provide the following options:
 We will have 5 arrays
 Array for employee name, employee hours , employee rate, employee salary, employee bonus
-Create a list of services provided for the user 
+Create a list of services provided for the user
 1- add hours to an employee
 2- add bonus to an employee
 3- calculate salary for an employee
@@ -16,12 +16,14 @@ Create a list of services provided for the user
 
 void add_hours(int hours1, char name_check, char names[5], int hours[5]);
 void add_bonus(int bonus1, char name_check, char names[5], int bonus[5]);
+void add_salary(char name_check,char names[5],int hours[5],int rate[5],int salary[5], int bonus[5]);
 int welcome_to_system();
+
 int main()
 {
     char names[5]={'A','B','C','D','E'};
     int hours[5]={0};
-    int rate[5]={0};
+    int rate[5]={10,10,10,10,10};
     int salary[5]={0};
     int bonus[5]={0};
     int choice;
@@ -46,6 +48,12 @@ int main()
         printf("plz enter the amount of bonus :");
         scanf ("%d",&bonus1);
         add_bonus(bonus1,names123,names,bonus);
+    }
+    else if (choice==3)
+    {
+        printf("plz enter the name of employee :");
+        scanf (" %c",&names123);
+        add_salary(names123,names,hours,rate,salary,bonus);
     }
     else if (choice==0) break;
 
@@ -91,4 +99,15 @@ void add_bonus(int bonus1, char name_check, char names[5], int bonus[5])
     }
     bonus[i]=bonus[i]+bonus1;
     printf ("bonus added !!\n");
+}
+void add_salary(char name_check,char names[5],int hours[5],int rate[5],int salary[5], int bonus[5])
+{
+    int i;
+    for (i=0;i<5;i++)
+    {
+        if (names[i]==name_check)
+            break;
+    }
+    salary[i]=hours[i]*rate[i]+bonus[i];
+    printf ("salary added !!\n");
 }
